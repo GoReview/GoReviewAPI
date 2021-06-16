2f15fce49bdfb01d51390d764d327e09fcf151d9
andrgab <andrgab@gmail.com>
2021-06-16 00:51:43 -0300

    Usuarios Controller

--- lib/goreviewapi_web/controllers/usuarios_controller.ex
+++ lib/goreviewapi_web/controllers/usuarios_controller.ex
@@ -16,16 +16,18 @@ defmodule GoreviewapiWeb.UsuariosController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    with {:ok, %Usuario{}} <- Goreviewapi.delete_user(id) do
+  def delete(conn, _params) do
+    with {:ok, logged_usuario} <- Guardian.current_usuario(conn),
+         {:ok, %Usuario{}} <- Goreviewapi.delete_usuario(logged_usuario) do
       conn
       |> put_status(:no_content)
       |> text("")
     end
   end
 
-  def index(conn, %{"id" => id}) do
-    with {:ok, %Usuario{} = usuario} <- Goreviewapi.get_user_by_id(id) do
+  def index(conn, _params) do
+    with {:ok, logged_usuario} <- Guardian.current_usuario(conn),
+         {:ok, %Usuario{} = usuario} <- Goreviewapi.get_user_by_id(logged_usuario) do
       conn
       |> put_status(:ok)
       |> render("usuario.json", usuario: usuario)
@@ -48,11 +50,23 @@ defmodule GoreviewapiWeb.UsuariosController do
     end
   end
 
-  def update(conn, params) do
-    with {:ok, %Usuario{} = usuario} <- Goreviewapi.update_user(params) do
+  def sign_in(conn, params) do
+    with {:ok, token} <- Guardian.authenticate(params) do
       conn
       |> put_status(:ok)
-      |> render("usuario.json", usuario: usuario)
+      |> render("sign_in.json", token: token)
+    end
+  end
+
+  def update(conn, params) do
+    with {:ok, logged_usuario} <- Guardian.current_usuario(conn) do
+      params = Map.put(params, "id", logged_usuario)
+
+      with {:ok, %Usuario{} = usuario} <- Goreviewapi.update_usuario(params) do
+        conn
+        |> put_status(:ok)
+        |> render("usuario.json", usuario: usuario)
+      end
     end
   end
 end
