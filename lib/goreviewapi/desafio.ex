999b35a152f26be7f525a249654c95aabbd0f6a9
Felipe Agricola <fen1499@gmail.com>
2021-06-15 22:50:33 -0300

    comentario, desafio, revisor e envio

---
+++ lib/goreviewapi/desafio.ex
@@ -0,0 +1,44 @@
+defmodule Goreviewapi.Desafio do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Goreviewapi.{Turma, Envio}
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  @required_params [:titulo, :descricao, :data_envio, :data_revisao]
+
+  @derive {Jason.Encoder, only: @required_params} #REVER ISSO AQUI
+
+  schema "desafios" do
+    field :titulo, :string
+    field :descricao, :string
+    field :data_envio, :date
+    field :data_revisao, :date
+
+    belongs_to :turma, Turma
+    has_many :envio, Envio, foreign_key: :desafio_id, references: :id
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> changes(params, @required_params)
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> changes(params, @required_params)
+  end
+
+  defp changes(struct, params, fields) do
+    struct
+    |> cast(params, fields)
+    |> validate_required(fields)
+    |> validate_length(:titulo, min: 6)
+    |> validate_number(:descricao, min: 6)
+  end
+
+end
---
+++ lib/goreviewapi/revisor.ex
@@ -0,0 +1,38 @@
+defmodule Goreviewapi.Revisor do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Goreviewapi.{Comentario, Usuario}
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  @required_params []
+
+  @derive {Jason.Encoder, only: @required_params++[:nota]} #REVER ISSO AQUI
+
+  schema "revisores" do
+    field :nota, :integer
+
+    has_many :comentario, Comentario
+    belongs_to :usuario, Usuario
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> changes(params, @required_params)
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> changes(params, @required_params)
+  end
+
+  defp changes(struct, params, fields) do
+    struct
+    |> cast(params, fields)
+    |> validate_required(fields)
+  end
+end
