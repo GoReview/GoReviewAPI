205770a9ac379a8a8ff09784e02d355b66251d07
Felipe Agricola <fen1499@gmail.com>
2021-06-17 20:12:33 -0300

    fix, alunos n carregava

--- lib/goreviewapi/turmas/get.ex
+++ lib/goreviewapi/turmas/get.ex
@@ -16,7 +16,7 @@ defmodule Goreviewapi.Turmas.Get do
         select: [:id, :name]
       )
 
-    case Repo.all(query) do
+    case Repo.all(query) |> Repo.preload(:usuario) do
       [] -> {:error, Error.build(:not_found, "Empty database")}
       turma -> {:ok, turma}
     end
