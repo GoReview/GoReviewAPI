ac342317f997642c9031257d74b8d338a74b37c4
andrgab <andrgab@gmail.com>
2021-06-17 02:12:47 -0300

    update

--- lib/goreviewapi_web/controllers/turmas_controller.ex
+++ lib/goreviewapi_web/controllers/turmas_controller.ex
@@ -8,38 +8,33 @@ defmodule GoreviewapiWeb.TurmasController do
   action_fallback FallbackController
 
   def create(conn, params) do
-    with  {:ok, %{"group" => "professor"}} <- Guardian.verify_claims(conn),
-          {:ok, %Turma{} = turma} <- Goreviewapi.create_turma(params)
-         do
+    with {:ok, %{"group" => "professor"}} <- Guardian.verify_claims(conn),
+         {:ok, %Turma{} = turma} <- Goreviewapi.create_turma(params) do
       conn
       |> put_status(:created)
       |> render("create.json", turma: turma)
     end
   end
 
-  #def list_por_aluno...
+  # def list_por_aluno...
 
-  #def index(conn, id) do...
-
-
+  # def index(conn, id) do...
 
   def show_list(conn, _params) do
-    with  {:ok, %{"group" => "professor"}} <- Guardian.verify_claims(conn),
-    {:ok, turma} <- Goreviewapi.get_turma do
+    with {:ok, %{"group" => "professor"}} <- Guardian.verify_claims(conn),
+         {:ok, turma} <- Goreviewapi.get_turma() do
       conn
       |> put_status(:ok)
       |> render("turma.json", turma: turma)
     end
-
   end
 
   def update(conn, params) do
-    with  {:ok, %{"group" => "professor"}} <- Guardian.verify_claims(conn),
-          {:ok, %Turma{} = turma} <- Goreviewapi.update_turma(params)
-         do
+    with {:ok, %{"group" => "professor"}} <- Guardian.verify_claims(conn),
+         {:ok, %Turma{} = turma} <- Goreviewapi.update_turma(params) do
       conn
       |> put_status(:ok)
-      |> render("turma.json", turma: turma)
+      |> render("turma_update.json", turma: turma)
     end
   end
 end
