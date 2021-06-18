043c71dc6473fc12b989092b96fdfaf7c711f995
andrgab <andrgab@gmail.com>
2021-06-17 22:05:05 -0300

    fix

--- lib/goreviewapi/revisores/update.ex
+++ lib/goreviewapi/revisores/update.ex
@@ -1,21 +1,21 @@
-defmodule Goreviewapi.Desafios.Update do
-  alias Goreviewapi.{Error, Desafio, Repo}
+defmodule Goreviewapi.Revisores.Update do
+  alias Goreviewapi.{Error, Revisor, Repo}
 
   def call(%{"id" => id} = params) do
-    case Repo.get(Desafio, id) do
-      nil -> {:error, Error.build_desafio_not_found_error()}
-      desafio -> do_update(desafio, params)
+    case Repo.get(Revisor, id) do
+      nil -> {:error, Error.build_revisor_not_found_error()}
+      revisor -> do_update(revisor, params)
     end
   end
 
-  defp do_update(desafio, params) do
-    desafio
-    |> Desafio.changeset(params)
+  defp do_update(revisor, params) do
+    revisor
+    |> Revisor.changeset(params)
     |> Repo.update()
     |> handle_update()
   end
 
-  defp handle_update({:ok, %Desafio{}} = result), do: result
+  defp handle_update({:ok, %Revisor{}} = result), do: result
 
   defp handle_update({:error, result}) do
     {:error, Error.build(:bad_request, result)}
