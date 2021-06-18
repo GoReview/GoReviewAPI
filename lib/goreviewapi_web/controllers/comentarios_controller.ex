7b1c9fea8bb8dc0f1cf23aa2408c92bd1f1c805a
Felipe Agricola <fen1499@gmail.com>
2021-06-18 20:55:06 -0300

    comentarios_controller

---
+++ lib/goreviewapi_web/controllers/comentarios_controller.ex
@@ -0,0 +1,27 @@
+defmodule GoreviewapiWeb.ComentariosController do
+  use GoreviewapiWeb, :controller
+
+  alias Goreviewapi.Comentario
+  alias GoreviewapiWeb.Auth.Guardian
+  alias GoreviewapiWeb.FallbackController
+
+  action_fallback FallbackController
+
+  def create(conn, params) do
+    with {:ok, %{"group" => "professor"}} <- Guardian.verify_claims(conn),
+    {:ok, %Comentario{} = comentario} <- Goreviewapi.create_comentario(params) do
+    conn
+    |> put_status(:created)
+    |> render("create.json", comentario: comentario)
+    end
+  end
+
+  def update(conn, params) do
+    with {:ok, %{"group" => "professor"}} <- Guardian.verify_claims(conn),
+         {:ok, %Comentario{} = comentario} <- Goreviewapi.update_comentario(params) do
+      conn
+      |> put_status(:ok)
+      |> render("comentario_update.json", comentario: comentario)
+    end
+  end
+end
